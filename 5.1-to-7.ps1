ecb1ecb450470e74c9b28d6dd879bed0d6256f1d
rahnoff <72708227+rahnoff@users.noreply.github.com>
2024-11-06 18:25:47 +0000

    Update 5.1-to-7.ps1

--- 5.1-to-7.ps1
+++ 5.1-to-7.ps1
@@ -1,3 +1,7 @@
 # On Windows Server 2016
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri 'https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.zip' -OutFile ''
+$destination = "$env:USERPROFILE"
+$powershellVersion = '7.4.6'
+$source = "https://github.com/PowerShell/PowerShell/releases/download/v$powershellVersion/PowerShell-$powershellVersion-win-x64.zip"
+$powershellArchive = $destination + $(Split-Path -Path $source -Leaf)
+Invoke-WebRequest -Uri $source -OutFile $powershellArchive
