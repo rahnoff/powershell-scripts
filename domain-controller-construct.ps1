dd78fc1e3f205c4407dbf04cfbf41490a206aef0
rahnoff <72708227+rahnoff@users.noreply.github.com>
2024-11-29 19:00:03 +0000

    Update domain-controller-construct.ps1

--- domain-controller-construct.ps1
+++ domain-controller-construct.ps1
@@ -23,7 +23,34 @@ $ADHT = @{
 }
 Install-ADDSForest @ADHT
 Restart-Computer
+
 # On the second one
 Resolve-DnsName -Name DC1 -Type A
 Test-NetConnection -ComputerName DC1 -Port 445
 Test-NetConnection -ComputerName DC1 -Port 389
+$FEATUREHT = @{
+    Name                   = 'AD-Domain-Services'
+    IncludeManagementTools = $True
+    WarningAction          = 'SilentlyContinue'
+}
+Install-WindowsFeature @FEATUREHT
+$ADMINDOMAIN = 'Administrator@DOMAIN.COM'
+$ADINSTALLHT = @{
+    String      = 'example'
+    AsPlainText = $True
+    Force       = $True
+}
+$SECUREPW = ConvertTo-SecureString @ADINSTALLHT
+$CRED = [PSCredential]::New($ADMINDOMAIN,$SECUREPW)
+$ADHT = @{
+    DomainName                    = 'DOMAIN.COM'
+    SafeModeAdministratorPassword = $SECUREPW
+    InstallDNS                    = $False
+    Force                         = $True
+    NoRebootOnCompletion          = $True
+    WarningAction                 = 'SilentlyContinue'
+    Credential                    = $CRED
+    SiteName                      = 'Default-First-Site-Name'
+    WarningAction                 = 'SilentlyContinue'
+}
+Install-ADDSDomainController @ADHT
